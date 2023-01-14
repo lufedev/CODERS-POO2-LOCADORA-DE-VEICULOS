31f8f25db7827820e0afcd0406d23d39ea5c6991
Natalia <nanademoura@gmail.com>
2023-01-13 23:05:40 -0300

    classe agencia

---
+++ agencia.java
@@ -0,0 +1,22 @@
+public abstract class agencia {
+
+  private String nome;
+  private String telefone;
+  private String email;
+  private String endereco;
+  private String veiculo;
+
+  public agencia(){}
+
+  public agencia(String nome, String telefone, String email, String endereco, String veiculo) {
+    this.nome = nome;
+    this.telefone = telefone;
+    this.email = email;
+    this.endereco = endereco;
+    this.veiculo = veiculo;
+  }
+
+  
+
+
+}
