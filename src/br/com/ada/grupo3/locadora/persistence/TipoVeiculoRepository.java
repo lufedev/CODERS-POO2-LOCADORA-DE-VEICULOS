ce85b4d3b92adda46ed509e50df52af7553e7f77
Ian Paschoal <104800595+IanZeta64@users.noreply.github.com>
2023-01-20 19:37:35 -0300

    salvarclientes/apenas arquivos clientes

--- src/br/com/ada/grupo3/locadora/persistence/TipoVeiculoRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.ada.grupo3.locadora.persistence;
-
-import br.com.ada.grupo3.locadora.model.TipoVeiculo;
-
-public interface TipoVeiculoRepository extends GenericRepository<TipoVeiculo> {
-}
--- src/br/com/ada/grupo3/locadora/persistence/TipoVeiculoRepositoryInMemory.java
+++
@@ -1,6 +0,0 @@
-package br.com.ada.grupo3.locadora.persistence;
-
-import br.com.ada.grupo3.locadora.model.TipoVeiculo;
-
-public class TipoVeiculoRepositoryInMemory extends GenericRepositoryInMemory<TipoVeiculo> implements TipoVeiculoRepository {
-}
